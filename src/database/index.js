417e6c542d0ebf8b2f1af9d2441b5ae361fd6b92
Rafael <Rafael.mattos468@gmail.com>
2024-06-02 19:49:48 -0300

    update

--- src/database/index.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-
-async function connectDatabase(){
-  await  mongoose.connect('mongodb+srv://rafaelmattos:root@cotapreco.vhirxgb.mongodb.net/?retryWrites=true&w=majority&appName=CotaPreco')
-}
-
-export default connectDatabase;
