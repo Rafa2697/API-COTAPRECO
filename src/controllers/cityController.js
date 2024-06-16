5a6a5b0a33bd0cbb3db74cf9ef74ce8533b3bae8
Rafael <Rafael.mattos468@gmail.com>
2024-06-16 05:42:20 -0300

    update

--- src/controllers/cityController.js
+++ src/controllers/cityController.js
@@ -1,9 +1,9 @@
 import City from '../models/cidade.js';
 
 async function getCity(request, response){
-  const city = request
+  const cities = request
 
-  return request
+  console.log(request)
 }
 
 async function createCity(request, response){
